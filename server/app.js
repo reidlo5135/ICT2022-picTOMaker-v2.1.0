19c07a73cd9a95ad534f0f90e9d71fbcb1b6f835
reidlo5135 <201840103@email.daelim.ac.kr>
2022-07-03 14:15:47 +0900

    07/03 14:15 REFACTOR: OAuth 로그인 API 개설

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 const port = 5000;
 
-var testRouter = require('./routes/routingTest/TestRouter');
+var OAuthRouter = require('./routes/oauth/OAuthRouter');
 
 var app = express();
 
@@ -16,12 +16,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/', testRouter);
+app.use('/', OAuthRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -30,11 +31,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
@@ -43,8 +42,8 @@ app.listen(port, () => {
   console.log(`Express app.js -> Connected to http://localhost:${port}`);
 });
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'client/build/index.html'));
+// });
 
 module.exports = app;
