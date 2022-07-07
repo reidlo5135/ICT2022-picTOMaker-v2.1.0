daaa8e370e36713585698d89779a6dd706e57e35
reidlo5135 <201840103@email.daelim.ac.kr>
2022-07-07 13:59:10 +0900

    07/07 13:58 REFACTOR: server app.js -> app.ts

---
+++ server/src/app.ts
@@ -0,0 +1,50 @@
+import express, {Request, Response, NextFunction} from 'express';
+import createError from 'http-errors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
+import bodyParser from "body-parser";
+import OAuthRouter from './routes/oauth/OAuthRouter';
+const {sequelize} = require('./models');
+// import {sequelize} from './models';
+
+const app: express.Application = express();
+const port = 5000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/', OAuthRouter);
+
+sequelize.sync({force:false})
+    .then(() => {
+        console.log('SEQ DB CONNECTED');
+    })
+    .catch((err:Error) => {
+        console.error(err);
+    });
+
+app.use(function(req:Request, res:Response, next:NextFunction) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err:any, req:Request, res:Response, next:NextFunction) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(port, () => {
+    console.log(`Express app.ts -> Connected to http://localhost:${port}`);
+});
+
+module.exports = app;
