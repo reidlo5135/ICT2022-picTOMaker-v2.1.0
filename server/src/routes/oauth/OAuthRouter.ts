a20458b987d9a161806b879efc5143222cf618bd
강준모 <201840103@email.daelim.ac.kr>
2022-07-07 22:34:42 +0900

    07/07 23:34 REFACTOR: Modify OAuth Logic functions

--- server/src/routes/oauth/OAuthRouter.ts
+++ server/src/routes/oauth/OAuthRouter.ts
@@ -1,8 +1,8 @@
-const express = require('express');
+import express from "express";
 const router = express.Router();
 const controller = require('../../controller/oauth/OAuthController');
 
 router.post('/v1/api/oauth2/token/:provider', controller.generateToken);
 router.post('/v1/api/oauth2/profile/:provider', controller.extractProfile);
 
-module.exports = router;
+export = router;
