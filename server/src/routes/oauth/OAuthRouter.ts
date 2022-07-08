c1b141c717ec8026ffd4a3a1e266dd72d0eb039a
reidlo5135 <201840103@email.daelim.ac.kr>
2022-07-08 12:22:18 +0900

    07/08 12:22 FEAT: OAuth Logout시 token invalid 기능 추가

--- server/src/routes/oauth/OAuthRouter.ts
+++ server/src/routes/oauth/OAuthRouter.ts
@@ -4,5 +4,6 @@ const controller = require('../../controller/oauth/OAuthController');
 
 router.post('/v1/api/oauth2/token/:provider', controller.generateToken);
 router.post('/v1/api/oauth2/profile/:provider', controller.extractProfile);
+router.delete('/v1/api/oauth2/token/invalid/:access_token', controller.invalidToken);
 
 export = router;
