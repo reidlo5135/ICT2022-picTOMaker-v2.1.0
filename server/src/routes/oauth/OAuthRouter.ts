002563a67dc74f14a25ec2e3a4f9f474b44542ac
reidlo5135 <201840103@email.daelim.ac.kr>
2022-07-09 15:20:30 +0900

    07/09 15:20 REFACTOR: OAuthRouter.ts 수정
    ROUTING 수정

--- server/src/routes/oauth/OAuthRouter.ts
+++ server/src/routes/oauth/OAuthRouter.ts
@@ -2,8 +2,8 @@ import express from "express";
 const router = express.Router();
 const controller = require('../../controller/oauth/OAuthController');
 
-router.post('/v1/api/oauth2/token/:provider', controller.generateToken);
-router.post('/v1/api/oauth2/profile/:provider', controller.extractProfile);
-router.delete('/v1/api/oauth2/token/invalid/:access_token', controller.invalidToken);
+router.post('/token/:provider', controller.generateToken);
+router.post('/profile/:provider', controller.extractProfile);
+router.delete('/token/invalid/:access_token', controller.invalidToken);
 
 export = router;
