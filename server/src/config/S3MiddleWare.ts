659eadb1e9d3bde1fb7dc215986f293e5dddf447
강준모 <201840103@email.daelim.ac.kr>
2022-07-12 21:30:42 +0900

    07/12 21:29 REFACTOR: S3MiddleWare.ts 수정

--- server/src/config/S3MiddleWare.ts
+++ server/src/config/S3MiddleWare.ts
@@ -1,16 +1,31 @@
-import multer from "multer";
-const s3 = require('../config/S3Secret');
+const multer = require('multer');
+const aws = require('aws-sdk');
 const multerS3 = require('multer-s3');
+const {s3Secret} = require('../config/S3Secret');
 
-export const upload = multer({
+aws.config.update({
+    accessKeyId: s3Secret.aws_access_key,
+    secretAccessKey: s3Secret.aws_secret_key,
+    region: s3Secret.region
+})
+const s3 = new aws.S3();
+
+const upload = multer({
     storage: multerS3({
         s3:s3,
         acl: 'public-read-write',
         bucket: 'ict2022-picto-bucket',
         contentType: multerS3.AUTO_CONTENT_TYPE,
+        metadata: function(req:any, file:any, cb:any) {
+            cb(null, {fieldName: file.fieldname});
+        },
         key: function (req:any, file:any, cb:any) {
-            cb(null, `${Date.now()}_${file.originalname}`);
+            cb(null, Math.floor(Math.random() * 1000).toString() + Date.now() + '.' + file.originalname.split('.').pop());
         }
     })
 });
+
+export = {
+    upload
+}
 
