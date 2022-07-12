2767f50314e98a4cc77302b3df25712a7dc0e985
reidlo5135 <201840103@email.daelim.ac.kr>
2022-07-12 11:21:15 +0900

    07/12 11:20 REFACTOR: After octet decode convert to image file

--- server/src/routes/s3/S3Router.ts
+++ server/src/routes/s3/S3Router.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import S3Controller from '../../controller/s3/S3Controller';
 const router = express.Router();
+const upload = require('../../config/S3MiddleWare');
 
 router.post('/register/:email/:provider', S3Controller.uploadImage);
+router.post('/register', upload.single('image'), S3Controller.registerS3);
 
 export = router;
--- server/src/service/s3/S3Service.ts
+++ server/src/service/s3/S3Service.ts
@@ -1,17 +1,27 @@
+const {writeFileSync, readFileSync} = require('fs');
 
 function decodeImage(params:any): Promise<any> {
     return new Promise<any>((resolve, reject) => {
         const octet = params.image.split(',');
-        console.log('S3 SVC octet : ', octet);
-        const decoded = Buffer.from(octet[1], 'base64').toString('utf-8');
+        console.log('S3 SVC octet 0 : ', octet[0]);
+        console.log('S3 SVC octet 1 : ', octet[1]);
+        const decoded = Buffer.from(octet[1], 'base64');
         console.log('S3 SVC decoded : ', decoded);
 
-        const blob = new Blob([decoded], {type:'image/png'});
-        console.log('S3 SVC blob : ', blob);
-        resolve(decoded);
+        writeFileSync('C:\\myPicTO\\test-image.png', decoded);
+        const file = readFileSync('C:\\myPicTO\\test-image.png');
+        console.log('S3 SVC decodeImage readFileSync file : ', file);
+        resolve(file);
+    });
+}
+
+function uploadS3(imageName:any): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+
     });
 }
 
 export = {
-    decodeImage
+    decodeImage,
+    uploadS3
 }
