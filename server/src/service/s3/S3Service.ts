9ef78fcca5be74f6ed5952d51ca80279251c6017
강준모 <201840103@email.daelim.ac.kr>
2022-07-12 21:30:06 +0900

    07/12 21:29 REFACTOR: delete file system logic

--- server/src/service/s3/S3Service.ts
+++ server/src/service/s3/S3Service.ts
@@ -2,26 +2,10 @@ const {writeFileSync, readFileSync} = require('fs');
 
 function decodeImage(params:any): Promise<any> {
     return new Promise<any>((resolve, reject) => {
-        const octet = params.image.split(',');
-        console.log('S3 SVC octet 0 : ', octet[0]);
-        console.log('S3 SVC octet 1 : ', octet[1]);
-        const decoded = Buffer.from(octet[1], 'base64');
-        console.log('S3 SVC decoded : ', decoded);
-
-        writeFileSync('C:\\myPicTO\\test-image.png', decoded);
-        const file = readFileSync('C:\\myPicTO\\test-image.png');
-        console.log('S3 SVC decodeImage readFileSync file : ', file);
-        resolve(file);
-    });
-}
-
-function uploadS3(imageName:any): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
 
     });
 }
 
 export = {
-    decodeImage,
-    uploadS3
+    decodeImage
 }
