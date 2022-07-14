23907407ed600e38dddc88607e635125698a5b7a
reidlo5135 <201840103@email.daelim.ac.kr>
2022-07-14 11:50:21 +0900

    07/14 11:50 FEAT: Add S3 Image Count By email

--- client/src/component/myPage/MyPage-profile.js
+++ client/src/component/myPage/MyPage-profile.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import "../../css/MyPage.css"
-import "../../css/font.css"
+import "../../css/MyPage.css";
+import "../../css/font.css";
+import axios from "axios";
 
 export default function MyPageProfile(){
     const getProfile = localStorage.getItem('profile');
@@ -9,13 +10,27 @@ export default function MyPageProfile(){
     const [email, setEmail] = useState();
     const [nickName, setNickName] = useState();
     const [profileImage, setProfileImage] = useState();
+    const [count, setCount] = useState(0);
 
     const getProf = () => {
         try {
             const jsonProf = JSON.parse(getProfile);
             console.log('MyPage-profile jProf : ', jsonProf);
 
-            setEmail(jsonProf.email);
+            const email = jsonProf.email;
+            setEmail(email);
+            axios.post(`/v1/api/picTO/count/${email}`)
+                .then((response) => {
+                    console.log('MyPage-profile getPicToCount : ', response.data);
+                    if(response.data.code === 0) {
+                        console.log('MyPage-profile getPicToCount count : ', response.data['count']);
+                        setCount(response.data['count']);
+                    }
+                })
+                .catch((e) => {
+                    console.error(e);
+                });
+
             if(provider === 'local') {
                 setNickName(jsonProf.nick_name);
             } else {
@@ -58,7 +73,7 @@ export default function MyPageProfile(){
                         제작한 픽토
                     </div>
                     <div className='MenuBox-props'>
-                        0
+                        {count}
                     </div>
                 </div>
             </div>
