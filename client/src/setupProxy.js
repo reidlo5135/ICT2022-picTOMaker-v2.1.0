42d65d5f4fc6fea228f40512e5bec709a74ee68a
reidlo5135 <201840103@email.daelim.ac.kr>
2022-07-16 14:59:13 +0900

    07/16 14:58 REFACTOR: setupProxy.js 수정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(createProxyMiddleware('/v1/api', {
-        target: 'http://ec2-54-180-19-153.ap-northeast-2.compute.amazonaws.com:5000/',
+        target: 'http://ec2-3-37-132-105.ap-northeast-2.compute.amazonaws.com:5000/',
         changeOrigin: true
     }));
 }
