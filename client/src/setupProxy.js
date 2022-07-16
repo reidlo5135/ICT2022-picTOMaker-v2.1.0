d6e9fd296f46c66c6de3d61108b9cfa9ec2338cd
reidlo5135 <201840103@email.daelim.ac.kr>
2022-07-16 12:00:32 +0900

    07/16 11:58 REFACTOR: proxy 수정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(createProxyMiddleware('/v1/api', {
-        target: 'http://localhost:5000',
+        target: 'http://ec2-54-180-19-153.ap-northeast-2.compute.amazonaws.com:5000/',
         changeOrigin: true
     }));
 }
