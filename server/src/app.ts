fa78bd65f16d2567ae1c4404e23da065ac6512ff
reidlo5135 <201840103@email.daelim.ac.kr>
2022-07-16 14:59:01 +0900

    07/16 14:58 REFACTOR: app.ts 수정

--- server/src/app.ts
+++ server/src/app.ts
@@ -52,10 +52,4 @@ app.listen(port, async () => {
         })
 });
 
-app.use(express.static(path.join(__dirname, '/client/build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
-
 module.exports = app;
